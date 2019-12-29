fe68070878ad1bf86d1fc7e752a561ac6176adf0
Daniele Pishinin <36366490+danipishinin@users.noreply.github.com>
2019-12-29 08:02:14 -0300

    Add files via upload

---
+++ Exercício Python #011 - Pintando Parede.py
@@ -0,0 +1,5 @@
+L = float(input('Digite a largura da parede:'))
+H = float(input('Digite a altura da parede da parede:'))
+
+print(f'Essa parede tem dimensão {L*H:.4f}m²')
+print(f'Para pintar essa parede, serão necessários {L*H/2:.4f}L de tinta.')
