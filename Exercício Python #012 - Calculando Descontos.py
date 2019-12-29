c170636fa5e2b66392ae89581a08376dc02621f1
Daniele Pishinin <36366490+danipishinin@users.noreply.github.com>
2019-12-29 08:11:35 -0300

    Add files via upload

---
+++ Exercício Python #012 - Calculando Descontos.py
@@ -0,0 +1,3 @@
+preco_produto = float(input('Digite o preço do produto'))
+desconto = preco_produto*5/100
+print(f'Com 5% de desconto, o produto vai de R${preco_produto} para R${preco_produto - desconto} ')
---
+++ Exercício Python #013 - Reajuste Salarial.py
@@ -0,0 +1,4 @@
+salario = float(input('Qual o salário do funcionário?'))
+reajuste = salario * 0.15
+
+print(f'Com o reajuste de 15%, o salário do funcionário passou de R${salario} à R${salario + reajuste:.2f}')
